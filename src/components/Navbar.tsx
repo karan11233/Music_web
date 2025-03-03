b4a911a6bdfe24ae62b85305e16b030c034a8ba4
karan11233 <karanchavda953@gmail.com>
2025-03-03 16:24:38 +0530

    complete

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,7 @@ import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 
+
 function Navbar({ className }: { className?: string }) {
   const [active, setActive] = useState<string | null>(null);
   return (
@@ -28,7 +29,7 @@ function Navbar({ className }: { className?: string }) {
             <HoveredLink href="/courses">Music Production</HoveredLink>
           </div>
         </MenuItem>
-        <Link href={"#"}>
+        <Link href= "/contect">
           <MenuItem
             setActive={setActive}
             active={active}
