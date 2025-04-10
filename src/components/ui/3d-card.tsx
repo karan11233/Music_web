665699086a65c993c52ab36ce3092db0b598d266
karan11233 <karanchavda953@gmail.com>
2025-04-10 14:34:09 +0530

    "c"

--- src/components/ui/3d-card.tsx
+++ src/components/ui/3d-card.tsx
@@ -33,12 +33,12 @@ export const CardContainer = ({
     const y = (e.clientY - top - height / 2) / 25;
     containerRef.current.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
   };
-
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
   const handleMouseEnter = (_e: React.MouseEvent<HTMLDivElement>) => {
     setIsMouseEntered(true);
     if (!containerRef.current) return;
   };
-
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
   const handleMouseLeave = (_e: React.MouseEvent<HTMLDivElement>) => {
     if (!containerRef.current) return;
     setIsMouseEntered(false);
